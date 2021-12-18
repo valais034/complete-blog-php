5f0f72053c639c5a3c5c36550f0dc5f5141414f4
User <valais034@gmail.com>
2021-12-18 14:17:56 +0330

    complete-blog-php

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ if (!$conn) {
     die("Error connecting to database: " . mysqli_connect_error());
 }
 // define global constants
-define ('ROOT_PATH', realpath(dirname(__FILE__)));
+define('ROOT_PATH', realpath(dirname(__FILE__)));
 define('BASE_URL', 'http://localhost/complete-blog-php/');
 ?>
